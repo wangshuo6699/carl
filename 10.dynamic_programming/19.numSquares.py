00f38d4d1a27fdf1d97e0bb76e32967887317313
shuowang10 <wangshuojack@163.com>
2023-08-25 08:14:47 +0800

    feat: 补全代码

--- 10.dynamic_programming/19.numSquares.py
+++ 10.dynamic_programming/19.numSquares.py
@@ -4,7 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [0] * (n+1)
+        dp = [float('inf')] * (n+1)
+        dp[0] = 0
+        for i in range(1, n+1):
+            for j in range(1, int(i**(1/2))+1):
+                if i>=j:
+                    dp[i] = min(dp[i], dp[i-j*j]+1)
+        return dp[-1]
 
 solution = Solution()
-solution.numSquares(12)
+res = solution.numSquares(12)
+print(res)
