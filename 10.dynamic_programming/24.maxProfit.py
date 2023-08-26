2b633b13453abf477b0c9889ec1051e4603dd675
wangshuojack6699 <wangshuojack@163.com>
2023-08-26 14:51:40 +0800

    feat:买卖股票的最佳时机I

---
+++ 10.dynamic_programming/24.maxProfit.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        dp = [[0, 0] for _ in prices]
+        dp[0][0] = -prices[0]
+        dp[0][1] = 0
+        for i in range(1, len(prices)):
+            dp[i][0] = max(dp[i-1][0], -prices[i])
+            dp[i][1] = max(dp[i-1][1], prices[i]+dp[i-1][0])
+        return dp[-1][-1]
+
+solution = Solution()
+res = solution.maxProfit([7,1,5,3,6,4])
+print(res)
