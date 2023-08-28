67c767d91c836cbda56e39157192056ede3e4dbf
shuowang10 <wangshuojack@163.com>
2023-08-28 08:23:10 +0800

    feat: 不相交的线

---
+++ 10.dynamic_programming/34.maxUncrossedLines.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxUncrossedLines(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        max_num = 0
+        dp = [[0]*(len(nums2)+1) for _ in range(len(nums1)+1)]
+        for i in range(1, len(nums1)+1):
+            for j in range(1, len(nums2)+1):
+                if nums1[i-1]==nums2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]+1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                max_num = max(max_num, dp[i][j])
+        return max_num
+
+solution = Solution()
+res = solution.maxUncrossedLines(nums1=[1,4,2], nums2=[1,2,4])
+print(res)
