21726d44deb2c752bd471e4f386081752ef4d4a0
shuowang10 <wangshuojack@163.com>
2023-09-01 21:50:58 +0800

    feat: 反转字符串

---
+++ 5.double_pointer/2.reverseString.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        i, j = 0, len(s)-1
+        while i<=j:
+            s[i], s[j] = s[j], s[i]
+            i+=1
+            j-=1
+        return s
+
+solution = Solution()
+res = solution.reverseString(s = ["h","e","l","l","o"])
+print(res)
