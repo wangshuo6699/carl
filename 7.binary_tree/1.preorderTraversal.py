c9f8b98af6d11aafe39cc0f9995f384d6b27bf33
shuowang10 <wangshuojack@163.com>
2023-09-03 12:00:39 +0800

    feat: 迭代法

--- 7.binary_tree/1.preorderTraversal.py
+++ 7.binary_tree/1.preorderTraversal.py
@@ -11,12 +11,26 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
+        # 递归法
+        # if not root:
+        #     return []
+        # left = self.preorderTraversal(root.left)
+        # right = self.preorderTraversal(root.right)
+        # return [root.val]+left+right
+
+        # 迭代法
         if not root:
             return []
-        
-        left = self.preorderTraversal(root.left)
-        right = self.preorderTraversal(root.right)
-        return [root.val]+left+right
+        stack = [root]
+        result = []
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return result
 
 node3 = TreeNode(3)
 node2 = TreeNode(2, node3, None)
