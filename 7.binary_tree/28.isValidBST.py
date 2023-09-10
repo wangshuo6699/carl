e118a5185fd34e1c39eba30b4d6efc19776cedb0
wangshuojack6699 <wangshuojack@163.com>
2023-09-10 10:17:39 +0800

    feat: 验证二叉搜索树

---
+++ 7.binary_tree/28.isValidBST.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        bt_list = self.inorderTraversal(root)
+        for i in range(len(bt_list)-1):
+            if bt_list[i]>=bt_list[i+1]:
+                return False
+        return True
+    
+    def inorderTraversal(self, root):
+        if not root:
+            return []
+        left = self.inorderTraversal(root.left)
+        right = self.inorderTraversal(root.right)
+        return left+[root.val]+right
+
+node1 = TreeNode(1)
+node2 = TreeNode(3)
+node3 = TreeNode(6)
+node4 = TreeNode(4, node2, node3)
+root  = TreeNode(5, node1, node4)
+# root  = TreeNode(5)
+
+solution = Solution()
+res = solution.isValidBST(root)
+print(res)
